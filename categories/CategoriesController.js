9b3317412832400b8fc766642254587bed062ba0
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2021-05-26 14:53:52 -0300

    Categories PATCH and POST working

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('./Category');
-const bcrypt = require('bcryptjs');
+const slugify = require('slugify');
 
 router.get('/', async (req, res) => {
     try{
@@ -39,95 +39,91 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// router.post('/', async(req, res) => {
-//     let { email, password } = req.body;
+router.post('/', async(req, res) => {
+    let { title } = req.body;
 
-//     if(email == undefined || password == undefined || email == '' || password == '') {
-//         res.status(400);
-//         res.json({
-//             error: 'Email or password invalid'
-//         });
-//     } else {
-//         let salt = bcrypt.genSaltSync();
-//         password = bcrypt.hashSync(password, salt);
-//         let user = await User.findOne({
-//             where: {
-//                 email
-//             }
-//         });
-//         if(user == undefined) {
-//             User.create({
-//                 email,
-//                 password
-//             }).then((user) => {
-//                 res.status(201);
-//                 res.json(user.get({
-//                     plain: true
-//                 }));
-//             }).catch(error => {
-//                 console.log(error);
-//                 res.sendStatus(500);
-//             })
-//         } else {
-//             res.status(409);
-//             res.json({
-//                 error: 'Email already exist'
-//             });
-//         }
-//     }
-// });
+    if(title == undefined || title == '') {
+        res.status(400);
+        res.json({
+            error: 'Title invalid'
+        });
+    } else {
+        let slug = slugify(title);
+        let category = await Category.findOne({
+            where: {
+                slug
+            }
+        });
+        if(category == undefined) {
+            Category.create({
+                title,
+                slug
+            }).then((category) => {
+                res.status(201);
+                res.json(category.get({
+                    plain: true
+                }));
+            }).catch(error => {
+                console.log(error);
+                res.sendStatus(500);
+            })
+        } else {
+            res.status(409);
+            res.json({
+                error: 'Already exists a category with similar title (same slug)'
+            });
+        }
+    }
+});
 
-// router.patch('/:id', async (req, res) => {
-//     // Maybe it would be viable to use multiple promises.
-//     // While one promise check the email, other promise will check if the id is valid or something.
-//     // Promises.all([promise1, promise2, ...promisen]).then((resolves[]) => resolves).catch(errors[] => errors);
-//     let id = req.params.id;
+router.patch('/:id', async (req, res) => {
+    // Maybe it would be viable to use multiple promises.
+    // While one promise check the email, other promise will check if the id is valid or something.
+    // Promises.all([promise1, promise2, ...promisen]).then((resolves[]) => resolves).catch(errors[] => errors);
+    let id = req.params.id;
 
-//     if(id == '' || isNaN(id)) {
-//         res.status(400);
-//         res.json({
-//             error: 'Invalid id'
-//         });
-//     } else {
-//         let { email, password } = req.body;
-//         if(email == undefined && password == undefined) {
-//             res.status(400);
-//             res.json({
-//                 error: 'You need to provide either email or password'
-//             });
-//         } else {
-            
-//                 let user = await User.findOne({ where: {id} });
+    if(id == '' || isNaN(id)) {
+        res.status(400);
+        res.json({
+            error: 'Invalid id'
+        });
+    } else {
+        let { title } = req.body;
+        if(title == undefined || title == '') {
+            res.status(400);
+            res.json({
+                error: 'Invalid title'
+            });
+        } else {
+            let category = await Category.findOne({ where: {id} });
 
-//                 if(user == undefined) {
-//                     res.status(404);
-//                     res.json({
-//                         error: `User with id ${id} not found`
-//                     });
-//                 } else {                    
-//                     let userByEmail = undefined;
+            if(category == undefined) {
+                res.status(404);
+                res.json({
+                    error: `Category with id ${id} not found`
+                });
+            } else {                 
+                let getCategoryBySlug = undefined;
 
-//                     if(email != undefined && email != '') {
-//                         user.email = email;
-//                         userByEmail = await User.findOne({ where: {email}});
-//                     }
+                getCategoryBySlug = await Category.findOne({ where: {slug: slugify(title)}});
 
-//                     if(userByEmail == undefined){ 
-//                         user.password = (password != undefined && password != '') ? bcrypt.hashSync(password, bcrypt.genSaltSync()) : user.password;
-                    
-//                         user.save();
-//                         res.json({
-//                             user
-//                         });   
-//                     } else {
-//                         res.status(409);
-//                         res.json({
-//                             error: 'The provided email is already in use'
-//                         });
-//                     }                           
-//             }
-//         }
-//     }
-// });
+                if(getCategoryBySlug == undefined){ 
+                    category.title = title;
+                    category.slug = slugify(title);
+                
+                    category.save();
+                    res.json({
+                        category
+                    });   
+                } else {
+                    res.status(409);
+                    res.json({
+                        error: 'Already exists a category with similar title (same slug)'
+                    });
+                }
+            }                
+        }
+    }
+});
 
 module.exports = router;
